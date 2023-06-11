a9655c29470f401b42ddadb577f693ca93e4ba21
KamenKoshutanski <kamen2109@gmail.com>
2023-06-11 21:51:05 +0300

    Basic functions

---
+++ script.js
@@ -0,0 +1,67 @@
+const taskForm = document.querySelector('#add-task');
+const taskList = document.querySelector('#tasks');
+const myTaskList = document.querySelector('#my-tasks');
+
+let tasks = [];
+
+function addTask(event) {
+    event.preventDefault();
+
+    const taskName = taskForm.querySelector('input[name="taskName"]').value;
+    const assignedTo = taskForm.querySelector('input[name="assignedTo"]').value;
+    const dueDate = taskForm.querySelector('input[name="dueDate"]').value;
+    console.log(taskName);
+    console.log(assignedTo);
+    console.log(dueDate);
+    const task = {
+        name: taskName,
+        assignedTo: assignedTo,
+        dueDate: dueDate
+    };
+
+    tasks.push(task);
+    
+
+    displayTasks();
+}
+
+function displayTasks() {
+    taskList.innerHTML = '';
+
+    tasks.forEach(task => {
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('task');
+        taskItem.innerHTML = `
+            <h3>${task.name}</h3>
+            <p>Assigned to: ${task.assignedTo}</p>
+            <p>Due Date: ${task.dueDate}</p>
+        `;
+
+        taskList.appendChild(taskItem);
+    });
+
+    displayMyTasks();
+}
+
+function displayMyTasks() {
+    const currentUser = 'JohnDoe';
+
+    const myTasks = tasks.filter(task => task.assignedTo === currentUser);
+
+    myTaskList.innerHTML = '';
+
+    myTasks.forEach(task => {
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('task');
+        taskItem.innerHTML = `
+            <h3>${task.name}</h3>
+            <p>Due Date: ${task.dueDate}</p>
+        `;
+
+        myTaskList.appendChild(taskItem);
+    });
+}
+
+taskForm.addEventListener('submit', addTask);
+
+displayTasks();
