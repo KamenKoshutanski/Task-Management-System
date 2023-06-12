331222ac6109e1f2eaa01ce3769b2967194b434d
KamenKoshutanski <kamen2109@gmail.com>
2023-06-12 17:53:31 +0300

    server load balancer

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-const app = express();
-const PORT = 3000;
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  const app = express();
+  const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join('frontend')));
+  app.use(express.static(path.join('frontend')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join('frontend', 'index.html'));
-});
+  app.get('/', (req, res) => {
+    res.sendFile(path.join('frontend', 'index.html'));
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
